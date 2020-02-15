5d06104512f72bad4a448f2224f0ef59fbe38b02
asdfhashg <asdfhashg>
2020-02-15 11:38:24 +0100

    random commit

--- player.py
+++ player.py
@@ -9,15 +9,21 @@ class Player:
             cards = player['hole_cards']
             cards += player['community_cards']
 
-            counter = Counter()
+            counter = collections.Counter()
             for card in cards:
+                if card['rank'] in ['J', 'K', 'Q', 'A']:
+                    card['rank'] = {'J': 11, 'Q': 12, 'K': 13, 'A': 14}[card['rank']]
+                else:
+                    card['rank'] = int(card['rank'])
                 counter[card['rank']] += 1
+            
+            cards.sort(key=lambda x: x['rank'])
 
             highest_card_count = 1
 
-            for i in range(2, 10) + ['J', 'K', 'Q', 'A']:
-                if counter[str(i)] > highest_card_count:
-                    highest_card_count = counter[str(i)]
+            for rank, count in counter.items():
+                if count > highest_card_count:
+                    highest_card_count = count
                     
             if highest_card_count == 2:
                 return game_state["current_buy_in"] - player['bet'] + game_state['minimum_raise']
@@ -31,4 +37,12 @@ class Player:
 
     def showdown(self, game_state):
         pass
+
+    def straight(self, cards):
+        ranks = set(cards.keys())
+
+        if len(ranks) < 5:
+            return False
+
+        return True
 
