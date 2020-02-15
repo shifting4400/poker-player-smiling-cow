2d76735dead202153581c8ca6030269e76cae15b
asdfhashg <asdfhashg>
2020-02-15 10:40:22 +0100

    second commit

--- player.py
+++ player.py
@@ -1,9 +1,22 @@
+import collections
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 1200
+        player = game_state['players'][game_state['in_action']]
+        cards = player['hole_cards']
+        cards += player['community_cards']
+
+        counter = Counter()
+        for card in cards:
+            counter[card['rank']] += 1
+
+        for i in range(2, 10) + ['J', 'K', 'Q', 'A']:
+            if counter[str(i)] > 1:
+                return 1200
+
+        return 0
 
     def showdown(self, game_state):
         pass
