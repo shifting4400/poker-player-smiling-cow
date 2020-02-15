d3e41be6df57e263acb5f5cf9685a5bea32c97ab
asdfhashg <asdfhashg>
2020-02-15 10:42:51 +0100

    random commit

--- player.py
+++ player.py
@@ -4,17 +4,21 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        player = game_state['players'][game_state['in_action']]
-        cards = player['hole_cards']
-        cards += player['community_cards']
+        try:
+            player = game_state['players'][game_state['in_action']]
+            cards = player['hole_cards']
+            cards += player['community_cards']
 
-        counter = Counter()
-        for card in cards:
-            counter[card['rank']] += 1
+            counter = Counter()
+            for card in cards:
+                counter[card['rank']] += 1
 
-        for i in range(2, 10) + ['J', 'K', 'Q', 'A']:
-            if counter[str(i)] > 1:
-                return 1200
+            for i in range(2, 10) + ['J', 'K', 'Q', 'A']:
+                if counter[str(i)] > 1:
+                    return 1200
+
+        except:
+            return 0
 
         return 0
 
